d180534eb07cbc0268e4a81e7498a9461d67e493
igorromanenko <webroma57@gmail.com>
2022-08-03 14:05:07 +0300

    added swiper scss, renamed folder templates to pug, changed some style structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const PATHS = {
-  src: path.join(__dirname, 'src/templates/views'),
+  src: path.join(__dirname, 'src/pug/views'),
 };
 const PAGES_DIR = PATHS.src;
 const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
